31784210ca5654ce7b9a35c3153c050e410241a4
Thomas Bjeldbak Madsen <75805327+Eshes1509@users.noreply.github.com>
2022-10-17 17:48:35 +0200

    Initial commit from Mac

---
+++ war-challenge/war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Thomas Bjeldbak Madsen on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
